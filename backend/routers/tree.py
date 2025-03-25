7e927a6684fca9f39a17787b94d0638a13c25fc7
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-03-25 19:59:15 -0300

    feat: get decision api

--- backend/routers/tree.py
+++ backend/routers/tree.py
@@ -1,4 +1,7 @@
+import typing
+
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from models.tree import DecisionTree
 
 blueprint_name = "decision_tree"
@@ -27,13 +30,56 @@ def create_or_update_tree(data: dict):
     return tree.tree
 
 
-@router.post("/decision")
-def decision(data: dict):
-    tree = DecisionTree.retrieve()
+@router.post("/execute")
+def execute(data: typing.List[typing.Dict]):
+    data_as_dict = {item.get("name"): item.get("value") for item in data}
 
+    tree = DecisionTree.retrieve()
+    print(data)
+    print(tree)
     if not tree:
         return {"error": "Decision tree not found!"}, 404
 
+    operations = {
+        "=": lambda a, b: a == b,
+        ">": lambda a, b: a > b,
+        "<": lambda a, b: a < b,
+        ">=": lambda a, b: a >= b,
+        "<=": lambda a, b: a <= b,
+    }
+
     current_node = tree.tree
 
-    print(current_node)
+    while current_node and current_node.get("type") != "end":
+        splitted_label = current_node.get("label").split(" ")
+
+        if len(splitted_label) != 3:
+            return JSONResponse(
+                content={
+                    "error": "Conditional must contain a field, a condition and a value"
+                },
+                status_code=400,
+            )
+
+        item_to_comp, condition, value = splitted_label
+
+        var_from_dict = data_as_dict.get(item_to_comp, "")
+        item_to_comp = (
+            float(var_from_dict) if var_from_dict.isdigit() else var_from_dict
+        )
+
+        value = float(value) if value.isdigit() else value
+
+        result = operations.get(condition, lambda a, b: None)(item_to_comp, value)
+
+        if result is None:
+            return JSONResponse(content={"error": "Invalid condition"}, status_code=400)
+
+        current_node = current_node.get("right" if result else "left")
+
+    if not current_node:
+        return JSONResponse(content={"decision": "Result not defined"}, status_code=200)
+
+    return JSONResponse(
+        content={"decision": current_node.get("label")}, status_code=200
+    )
