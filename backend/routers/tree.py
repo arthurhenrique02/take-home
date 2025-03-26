307e7ead2ea4aeed058fa34796db54d51f0293f4
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-03-25 22:20:33 -0300

    chore: added some comments. Handling empty label

--- backend/routers/tree.py
+++ backend/routers/tree.py
@@ -66,23 +66,26 @@ def execute(data: typing.List[typing.Dict]):
                 status_code=400,
             )
 
+        # get the field, condition and value from the label
+        # check if fields/values are numbers and convert them to numbers
         item_to_comp, condition, value = splitted_label
 
         var_from_dict = data_as_dict.get(item_to_comp, "")
         item_to_comp = (
             float(var_from_dict) if var_from_dict.isdigit() else var_from_dict
         )
-
         value = float(value) if value.isdigit() else value
 
         result = operations.get(condition, lambda a, b: None)(item_to_comp, value)
 
+        # if None, the condition was not passed correctly
         if result is None:
             return JSONResponse(content={"error": "Invalid condition"}, status_code=400)
 
         current_node = current_node.get("right" if result else "left")
 
-    if not current_node:
+    # if not end node found or node label is empty
+    if not current_node or current_node.get("label") in ["", None, "end"]:
         return JSONResponse(content={"decision": "Result not defined"}, status_code=200)
 
     return JSONResponse(
